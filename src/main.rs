ac4f1e7b579ed8db79ef689b03b216c96b9d8980
Imran Ahmad <meetimrang@gmail.com>
2023-09-05 21:14:30 +0500

    added loop for multiple tries

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,28 @@ fn main() {
     println!("Guess the Number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    
-    println!("Enter your guess:");
-    let mut guess = String::new();
-
-    io::stdin()
-      .read_line(&mut guess)
-      .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    //println!("You guessed: {guess}");
-    println!("You guessed {}", &guess);
-
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
-    }
-
     println!("The secret number was: {secret_number}");
 
+   
+    loop{
+        println!("Enter your guess:");
+        let mut guess = String::new();
+
+        io::stdin()
+          .read_line(&mut guess)
+          .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        //println!("You guessed: {guess}");
+        println!("You guessed {}", &guess);
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
+
+    }
+    
 }
